6d3d47c2656949b60ed80a8be19d4d5e3fa34265
Raitis Cerkasovs <raitis.cerkasovs@gmail.com>
2015-11-24 18:09:09 +0000

    stage 2

--- platforms/windows/www/cordova_plugins.js
+++ platforms/windows/www/cordova_plugins.js
@@ -1,4 +1,4 @@
-﻿cordova.define('cordova/plugin_list', function(require, exports, module) {
+cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
